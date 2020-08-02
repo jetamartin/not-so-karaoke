266b787fd65919c6f2140005c203e99fb5635a3a
Jet Martin <jetamartin@gmail.com>
2020-08-02 11:47:58 -0700

    added 404.html page and styled it.

--- app.py
+++ app.py
@@ -38,6 +38,10 @@ connect_db(app)
 print("*********** H E L L O *****************")
 # videos = []
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
 @app.route('/')
 def home():
   return render_template("/index.html")
