e2ad23ffc71632551248aa3f8026c58c98747457
Jet Martin <jetamartin@gmail.com>
2020-08-04 15:12:42 -0700

    back to second solution resetting user agent

--- utilities.py
+++ utilities.py
@@ -39,15 +39,11 @@ def get_lyrics(artist,song_title):
   print(url)
   
   try:
-    opener = AppURLopener()
-    # content = opener.open(url)
-    content = opener.open(url).read()
 
-    
     #  Manually set a user agent to avoid server's web security (e.g., mod_security) that may be preventing scraping
     # see stackoverflow: https://stackoverflow.com/questions/16627227/http-error-403-in-python-3-web-scraping 
-    # req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    # content = urlopen(req, timeout=10).read()
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    content = urlopen(req, timeout=10).read()
 
     # content = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(content, 'html.parser')
